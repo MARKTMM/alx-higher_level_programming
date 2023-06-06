cdcad9027509da0ee62005c85de9eefa86bb67e2
MARKTMM <markmwangi304@gmail.com>
2023-06-06 14:17:34 -0700

    fixed some errors

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for l in range(ord('a'), ord('z')+1):
-    if l is not (ord('q')) and l is not (ord('e')):
-        print('{}'.format(chr(l)), end='')
+for letter in range(ord('a'), ord('z')+1):
+    if letter not in (ord('q'), ord('e')):
+        print(chr(letter), end="")
